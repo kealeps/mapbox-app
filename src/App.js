0bde576072115b6ad09479f69c91ad36afb9d9d4
Kevin <mr.kv17.kp@gmail.com>
2022-03-26 15:43:01 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Map from './components/Map'
+
+function App() {
+  return (
+    <div className="App">
+      <div>Haystack Map</div>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
